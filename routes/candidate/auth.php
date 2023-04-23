df6c90b6526dfbc292e8fbfcbd51b225e74b2ad9
thinhdang <thinh08012000@gmail.com>
2023-04-23 08:06:48 +0700

    [Nika] edit dir route

--- routes/candidate/auth.php
+++ routes/candidate/auth.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Candidate\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Candidate\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Candidate\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Candidate\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Candidate\Auth\NewPasswordController;
+use App\Http\Controllers\Candidate\Candidate\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Candidate\Auth\RegisteredUserController;
+use App\Http\Controllers\Candidate\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
